57fe48dae10b37e8dfa219aa4d63886173b936d1
Usman Babakolo <usmansbk@gmail.com>
2021-07-10 16:15:46 +0100

    Setup css loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import "./style.css";
+
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = "Welcome to Microverse";
+  element.classList.add("hello");
 
   return element;
 }
