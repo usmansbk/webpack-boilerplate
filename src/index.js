89ac748ab92e0ab9a02efd9f12cf890031fbb8ab
Usman Suleiman <usmansbk@gmail.com>
2021-07-27 12:47:39 +0100

    Change file structure

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import app from './app.html';
+import './style.css';
+
+const root = document.getElementById('root');
+root.innerHTML = app;
--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import root from '../root.html';
-import '../style.css';
-
-document.body.innerHTML = root;
